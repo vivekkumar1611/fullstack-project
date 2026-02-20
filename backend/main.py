af49b34237262d9d796c2894876163c4c6e21398
vivekkumar1611 <k.vivekk1@gmail.com>
2026-02-20 16:04:01 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal
+import models
+from database import engine
+from models import Base
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
+# DB connection
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Root API
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI!"}
+    return {"message": "FastAPI connected to PostgreSQL"}
+
+# Get all users
+@app.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
