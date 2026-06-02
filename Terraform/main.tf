278e10f361e9dcf397a8c608d5f63cba0f97c3f7
vivekkumar1611 <k.vivekk1@gmail.com>
2026-06-02 11:19:07 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,3 +8,22 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
+
+EC2:
+resource "aws_instance" "fullstack" {
+
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  key_name = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.fullstack_sg.id
+  ]
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "fullstack-server"
+  }
+}
